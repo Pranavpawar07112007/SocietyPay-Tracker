48c5679a52f2c883de99025eca6f50dd1262ae81
Pranav Pawar <pranav07112007@gmail.com>
2025-08-19 10:58:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardTitle classNa

--- src/app/receipt/[paymentId]/page.tsx
+++ src/app/receipt/[paymentId]/page.tsx
@@ -121,7 +121,7 @@ export default function ReceiptPage() {
                 <CardHeader className="border-b border-border text-center">
                     <div className="flex justify-center items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-primary"><path d="M9 12h6m-6 4h6m2 5H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5.586a1 1 0 0 1 .707.293l5.414 5.414a1 1 0 0 1 .293.707V19a2 2 0 0 1-2 2Z"/></svg>
-                        <CardTitle className="font-headline text-3xl">
+                        <CardTitle className="font-headline text-2xl">
                             Maintenance Receipt
                         </CardTitle>
                     </div>
