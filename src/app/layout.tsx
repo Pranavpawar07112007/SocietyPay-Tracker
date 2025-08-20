84eb4b19bcc581662e2b4c0c54d6777739591c8b
Pranav Pawar <pranav07112007@gmail.com>
2025-08-20 06:17:00 +0000

    nothing happened

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange
            >
             <AuthProvider>
                 <main className="animate-fade-in">
